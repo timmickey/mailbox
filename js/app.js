731988eb5d0c847bbea1a1cbb54d084e5c807e12
tim mickey <tim@holdmyticket.com>
2014-03-24 19:23:39 -0600

    add some files

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//http://holdmyticket.com/api/public/events/nearby/location/Albuquerque,%20NM/lat/35.0845/lng/-106.6511/flyers_only/y/accuracy/25/api_key/anon
+
+//http://holdmyticket.com/api/public/events/nearby/location/Albuquerque,%20NM/lat/35.0845/lng/-106.6511/flyers_only/y/accuracy/25/page/1/api_key/anon
+
+App = Ember.Application.create();
+
+App.Router.map(function(){
+
+});
+
+App.IndexRoute = Ember.Route.extend({
+
+});
