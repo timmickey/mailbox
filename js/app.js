23f654d79cb5e991d45c9b1de28376b2d863f0f5
tim mickey <tim@holdmyticket.com>
2014-03-24 19:25:56 -0600

    change up file names

--- js/app.js
+++ js/app.js
@@ -1,13 +1,39 @@
-//http://holdmyticket.com/api/public/events/nearby/location/Albuquerque,%20NM/lat/35.0845/lng/-106.6511/flyers_only/y/accuracy/25/api_key/anon
+// Model
 
-//http://holdmyticket.com/api/public/events/nearby/location/Albuquerque,%20NM/lat/35.0845/lng/-106.6511/flyers_only/y/accuracy/25/page/1/api_key/anon
+App.Mailbox = Em.Object.extend();
 
-App = Ember.Application.create();
-
-App.Router.map(function(){
-
+App.Mailbox.reopenClass({
+  find: function(id) {
+    if (id) {
+      return App.FIXTURES.findBy('id', id);
+    } else {
+      return App.FIXTURES;
+    }
+  }
 });
 
-App.IndexRoute = Ember.Route.extend({
+// Routes
 
+App.Router.map(function() {
+  this.resource('mailbox', { path: '/:mailbox_id' }, function() {
+    this.resource('mail', { path: '/:message_id' });
+  });
+});
+
+App.ApplicationRoute = Em.Route.extend({
+  model: function() {
+    return App.Mailbox.find();
+  }
+});
+
+App.MailRoute = Em.Route.extend({
+  model: function(params) {
+    return this.modelFor('mailbox').messages.findBy('id', params.message_id);
+  }
+});
+
+// Handlebars helper
+
+Ember.Handlebars.registerBoundHelper('date', function(date) {
+  return moment(date).format('MMM Do');
 });
