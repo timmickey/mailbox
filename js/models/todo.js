54a8e96dfd03c737e7d6e5abca77fc0f50a06de6
Wes <wes@holdmyticket.com>
2014-03-25 18:58:17 -0600

    major cleanup and getting rid of shit

---
+++ js/models/todo.js
@@ -0,0 +1,12 @@
+/*global Todos, DS */
+'use strict';
+
+Todos.Todo = DS.Model.extend({
+	title: DS.attr('string'),
+	isHeader: DS.attr('boolean'),
+	isCompleted: DS.attr('boolean'),
+	isRemoved: DS.attr('boolean'),
+	saveWhenCompletedChanged: function () {
+		this.save();
+	}.observes('isCompleted')
+});
